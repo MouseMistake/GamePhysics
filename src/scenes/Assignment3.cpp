e22d0e9253abe88b7c73591fe0552e5b2cd43707
MouseMistake <lena.tari@gmail.com>
2025-04-14 21:37:21 +0200

    Blank Assignment 3

---
+++ src/scenes/Assignment3.cpp
@@ -0,0 +1,16 @@
+#include "Assignment3.h"
+
+#include "imgui.h"
+
+void Assignment3::OnEnable() {}
+
+void Assignment3::OnDisable() {}
+
+void Assignment3::Update(float deltaTime) {}
+
+void Assignment3::Draw() {}
+
+void Assignment3::DrawGUI() {
+    ImGui::Begin("Inspector");
+    ImGui::End();
+}
